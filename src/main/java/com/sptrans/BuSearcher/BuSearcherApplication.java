7ddfd42460f4a77d7b3aec4af5ad4c593cd572d7
Samuel Monte <samuel.monte@flexdoc.local>
2023-10-27 10:20:50 -0300

    spring project

---
+++ src/main/java/com/sptrans/BuSearcher/BuSearcherApplication.java
@@ -0,0 +1,13 @@
+package com.sptrans.BuSearcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuSearcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuSearcherApplication.class, args);
+	}
+
+}
